de3ae828e177a549b8a059528ddacbbac82fece0
skarvounis <skarvounis@net.geoanalysis.gr>
2022-12-15 15:46:48 +0200

    Final Content. Addition of event registration section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const getBtn = document.getElementById("open-acc");
 const wantedCryptos = ["BTC", "ETH", "XRP", "LTC", "BCH"];
 const sendHttpRequest = () => {
   const promise = new Promise((resolve, reject) => {
@@ -20,14 +19,16 @@ const getData = () => {
   sendHttpRequest().then((responseData) => {
     let data = responseData.data;
     const result = data.filter((item) => wantedCryptos.includes(item.symbol));
-    result.forEach(element => {
-      console.log(element.symbol)
-      document.getElementById(`${element.nameid}-symbol`).innerHTML =
-        `${element.symbol}`;
+    result.forEach((element) => {
+      console.log(element.symbol);
+      document.getElementById(
+        `${element.nameid}-symbol`
+      ).innerHTML = `${element.symbol}`;
       document.getElementById(`${element.nameid}-name`).innerHTML =
         element.nameid.toUpperCase();
-      document.getElementById(`${element.nameid}-price`).innerHTML =
-        `$ ${Number(element.price_usd).toLocaleString('en-US')}`;
+      document.getElementById(
+        `${element.nameid}-price`
+      ).innerHTML = `$ ${Number(element.price_usd).toLocaleString("en-US")}`;
       if (Number(element.percent_change_24h) < 0) {
         document
           .getElementById(`${element.nameid}-tra`)
@@ -36,16 +37,114 @@ const getData = () => {
           .getElementById(`${element.nameid}-tra`)
           .classList.add("text-red-500");
       }
-      document.getElementById(`${element.nameid}-tra`).innerHTML =
-        `${element.percent_change_24h}%`;
+      document.getElementById(
+        `${element.nameid}-tra`
+      ).innerHTML = `${element.percent_change_24h}%`;
     });
   });
 };
+
 window.onload = () => {
   getData();
+  document.getElementById("mail").value = "";
+  document.getElementById("password").value = "";
 };
-getBtn.addEventListener("click", getData);
 
-function setValues(result) {
-  r;
+function validateEmail() {
+  const email = document.getElementById("mail").value;
+  //   console.log(pw)
+  //   if (pw == "") {
+  //   }
+}
+
+const pw = document.getElementById("password");
+
+function initializeValidation(x) {
+  const lenght = document.getElementById("length");
+  const numbers = document.getElementById("numbers");
+  const lower = document.getElementById("lowercase");
+  const upper = document.getElementById("uppercase");
+  const special = document.getElementById("special");
+
+  const validators = [lenght, numbers, lower, upper, special];
+  validators.forEach((element) => {
+    element.classList.remove("text-white");
+    element.classList.add("text-red-500");
+  });
+  // pw.classList.add("border border-red-500");
+}
+
+function validatePassword(x) {
+  const lenght = document.getElementById("length");
+  const numbers = document.getElementById("numbers");
+  const lower = document.getElementById("lowercase");
+  const upper = document.getElementById("uppercase");
+  const special = document.getElementById("special");
+
+  const value = pw.value;
+  if (value.length >= 8) {
+    lenght.classList.remove("text-red-500");
+    lenght.classList.add("text-green-500");
+  } else {
+    lenght.classList.add("text-red-500");
+    lenght.classList.remove("text-green-500");
+  }
+
+  let lowerCaseLetters = /[a-z]/g;
+  if (value.match(lowerCaseLetters)) {
+    lower.classList.remove("text-red-500");
+    lower.classList.add("text-green-500");
+  } else {
+    lower.classList.add("text-red-500");
+    lower.classList.remove("text-green-500");
+  }
+
+  var upperCaseLetters = /[A-Z]/g;
+  if (value.match(upperCaseLetters)) {
+    upper.classList.remove("text-red-500");
+    upper.classList.add("text-green-500");
+  } else {
+    upper.classList.add("text-red-500");
+    upper.classList.remove("text-green-500");
+  }
+  var nms = /[0-9]/g;
+  if (value.match(nms)) {
+    numbers.classList.remove("text-red-500");
+    numbers.classList.add("text-green-500");
+  } else {
+    numbers.classList.add("text-red-500");
+    numbers.classList.remove("text-green-500");
+  }
+
+  let char = /[\^!@#\$%\^\&*\)\(\[\]\/\\?+=,._-]+$/g;
+  if (value.match(char)) {
+    special.classList.remove("text-red-500");
+    special.classList.add("text-green-500");
+  } else {
+    special.classList.add("text-red-500");
+    special.classList.remove("text-green-500");
+  }
+
+  if (
+    value.length >= 8 &&
+    value.match(lowerCaseLetters) &&
+    value.match(upperCaseLetters) &&
+    value.match(nms) &&
+    char.test(value)
+  ) {
+    pw.classList.add("text-green-500");
+    pw.classList.remove("text-red-500");
+
+    document.getElementById("submit").classList.add("bg-green-500");
+    document.getElementById("submit").classList.add("bg-slate-400");
+    document.getElementById("submit").classList.add("cursor-pointer");
+    document.getElementById("submit").classList.add("cursor-not-allowed");
+    document.getElementById("submit").disabled = false;
+  }
+}
+
+function Submit(event) {
+  event.preventDefault();
+  document.getElementById("congrats").classList.remove("hidden");
+  document.getElementById("congrats").classList.add("flex");
 }
